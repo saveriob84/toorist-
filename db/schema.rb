78ab4226ec7cf09c625f9024d2deadd8effa0867
saveriob84 <savebutera@hotmail.it>
2017-08-03 14:54:27 +0200

    ceato contenuti

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801130802) do
+ActiveRecord::Schema.define(version: 20170803123926) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string   "titolo"
+    t.text     "descrizione"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
